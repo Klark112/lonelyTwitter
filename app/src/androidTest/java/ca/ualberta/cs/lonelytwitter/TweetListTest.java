80a17db68ddf05d7ec4b28917a8fa594e83570c3
klark112 <klark@ualberta.ca>
2016-01-26 19:59:36 -0700

    finished lab 4

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,6 +1,10 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by klark on 1/26/16.
  */
@@ -37,6 +41,7 @@ public class TweetListTest extends ActivityInstrumentationTestCase2{
         tweets.add(tweet);
         tweets.delete(tweet);
 
+
         assertFalse(tweets.hasTweet(tweet));
     }
 
@@ -51,6 +56,51 @@ public class TweetListTest extends ActivityInstrumentationTestCase2{
         assertEquals(returnedTweet.getDate(),tweet.getDate());
     }
 
+    public void testgetCount(){
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        Tweet tweet = new NormalTweet("Test Tweet");
+        int arrayplace = tweets.size();
+        assertEquals(0,arrayplace);
+        tweets.add(tweet);
+        arrayplace+=1;
+        assertEquals(1,arrayplace);
+    }
+
+    public void testhasTweet2(){
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        assertTrue(tweets.isEmpty());
+    }
+
+    public ArrayList testgetTweets(){
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        assertFalse(tweets.size()==0);
+        tweets.add(new NormalTweet("Test Tweet1"));
+        tweets.add( new NormalTweet("Test Tweet2"));
+        tweets.add( new NormalTweet("Test Tweet3"));
+        tweets.add( new NormalTweet("Test Tweet4"));
+        tweets.add( new NormalTweet("Test Tweet5"));
+        tweets.add( new NormalTweet("Test Tweet6"));
+        assertTrue(tweets.size()>2);
+        for(int i=1;i<tweets.size();i++){
+            Date date1=tweets.get(i-1).getDate();
+            Date date2=tweets.get(i).getDate();
+            assertTrue(date1.before(date2));
+        }
+        return tweets;
+    }
+    public void testremoveTweet() { //-- should remove a tweet
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("Tweet1");
+        tweets.add(tweet);
+        tweets.removeTweet(tweet);
+        assertFalse(tweets.hasTweet(tweet));
+    }
+    public void testaddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("Tweet1");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
 
 
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,6 +1,9 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 
 /**
  * Created by klark on 1/26/16.
@@ -24,11 +27,43 @@ public class TweetList {
         tweets.remove(tweet);
     }
 
-    public void addTweet(){} //-- should throw an IllegalArgumentException when one tries to add a duplicate tweet
-    public void getTweets(){} //-- sould return a list of tweets in chronological order
-    public void hasTweet(){} //-- should return true if there is a tweet that equals() another tweet
-    public void removeTweet(){} //-- should remove a tweet
-    public void getCount(){} //-- should accurately count up the tweets
+
+    //start of lab 4
+    public void addTweet(Tweet tweet){ //-- should throw an IllegalArgumentException when one tries to add a duplicate tweet
+
+        if (tweets.contains(tweet)){
+            throw new IllegalArgumentException();
+        }
+        tweets.add(tweet);
+    }
+    public ArrayList<Tweet> getTweets(){ //-- should return a list of tweets in chronological order
+        ArrayList<Tweet> sortingtweets=tweets;
+        Collections.sort(sortingtweets, new Comparator<Tweet>(){
+            public int compare (Tweet t1, Tweet t2){
+                return t1.getDate().compareTo(t2.getDate());
+            }
+        });
+        return tweets;
+    }
+
+    public boolean hasTweet2(Tweet tweet){ //-- should return true if there is a tweet that equals() another tweet
+
+        if(tweets.contains(tweet)){
+            return true;
+        }
+        return false;
+        /*else{
+            return false;
+        }*/
+    }
+    public void removeTweet(Tweet tweet){ //-- should remove a tweet
+
+        tweets.remove(tweet);
+    }
+    public void getCount(){ //-- should accurately count up the tweets
+        int arrayplace = tweets.size();
+        System.out.format("%d",arrayplace);
+    }
 
 
 }
